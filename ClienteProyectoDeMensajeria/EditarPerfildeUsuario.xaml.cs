da532d4494091c833df329f2eaa72f74ae20a1c7
irasemcai <36616732+irasemcai@users.noreply.github.com>
2021-01-03 02:01:06 -0600

    pantallas base

---
+++ ClienteProyectoDeMensajeria/EditarPerfildeUsuario.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienteProyectoDeMensajeria
+{
+    /// <summary>
+    /// Lógica de interacción para EditarPerfildeUsuario.xaml
+    /// </summary>
+    public partial class EditarPerfildeUsuario : UserControl
+    {
+        public EditarPerfildeUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
