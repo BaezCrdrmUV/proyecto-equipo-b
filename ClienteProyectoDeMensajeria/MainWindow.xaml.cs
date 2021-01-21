7f825de7be7bd69c1b845ed052b18d6bbc5d444d
Batmanelshido21 <javierjuarez.jj28@gmail.com>
2021-01-20 22:59:39 -0600

    prueba de conexion

--- ClienteProyectoDeMensajeria/MainWindow.xaml.cs
+++ ClienteProyectoDeMensajeria/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,17 +29,38 @@ namespace ClienteProyectoDeMensajeria
 
         private void iniciarSesion(object sender, RoutedEventArgs e)
         {
-            string correoUsuario = correoU.Text.Trim();
-            string contraseñaUsuario = contraseñaU.Password.Trim();
-            
-            if (ValidarDatosIngresados(correoUsuario, contraseñaUsuario))
+            var url = $"http://localhost:5000/estado";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            MessageBox.Show("Entró al método");
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = (senderX, certificate, chain, sslPolicyErrors) => { return true; };
+            try
             {
-                MessageBox.Show("Datos correctos");
-                
+                MessageBox.Show("Entró al try");
+                using (WebResponse response = request.GetResponse())
+                {
+                    MessageBox.Show("Primer using");
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        MessageBox.Show("Segundo using");
+                        if (strReader == null) return;
+                        MessageBox.Show("Despues del if");
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            MessageBox.Show("Último using");
+                            string responseBody = objReader.ReadToEnd();
+                            // Do something with responseBody
+                            Console.WriteLine(responseBody);
+                            MessageBox.Show(responseBody);
+                        }
+                    }
+                }
             }
-            else
+            catch (WebException ex)
             {
-                MessageBox.Show("Datos invalidos");
+                MessageBox.Show(ex+"");
             }
 
         }
