73ed0a192b39fbbfb63de74783b585e71906a47a
Batmanelshido21 <javierjuarez.jj28@gmail.com>
2021-01-22 05:10:04 -0600

    login funcional

---
+++ ServiciosCuentaUsuario/IServicioCuentaUsuario.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiciosCuentaUsuario
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioCuentaUsuario
+    {
+        [OperationContract]
+        Cuenta IniciarSesion(string correo, string contrasena);
+
+        [OperationContract]
+        MensajeR RegistrarUsuario(Cuenta cuenta, Contrasena contrasena, Correo correo, Telefono telefono);
+        
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    // Puede agregar archivos XSD al proyecto. Después de compilar el proyecto, puede usar directamente los tipos de datos definidos aquí, con el espacio de nombres "ServiciosCuentaUsuario.ContractType".
+    [DataContract]
+    public class Cuenta
+    {
+        [DataMember]
+        int idCuenta;
+        [DataMember]
+        string nombreUsuario;
+        [DataMember]
+        int idFotoCuentaUsuario;
+        [DataMember]
+        int Genero_idGenero;
+
+
+        public Cuenta(int idCuenta, string nombreUsuario, int idFotoCuentaUsuario, int genero_idGenero)
+        {
+            this.idCuenta = idCuenta;
+            this.nombreUsuario = nombreUsuario;
+            this.idFotoCuentaUsuario = idFotoCuentaUsuario;
+            Genero_idGenero = genero_idGenero;
+        }
+        public Cuenta(int idCuenta, string nombreUsuario, int genero_idGenero)
+        {
+            this.idCuenta = idCuenta;
+            this.nombreUsuario = nombreUsuario;
+            Genero_idGenero = genero_idGenero;
+        }
+    }
+
+    [DataContract]
+    public class Contrasena
+    {
+        [DataMember]
+        int idContrasena;
+        [DataMember]
+        string contrasena;
+        [DataMember]
+        int Cuenta_idCuenta;
+
+        public Contrasena(int idContrasena, string contrasena, int cuenta_idCuenta)
+        {
+            this.idContrasena = idContrasena;
+            this.contrasena = contrasena;
+            Cuenta_idCuenta = cuenta_idCuenta;
+        }
+    }
+    [DataContract]
+    public class Correo
+    {
+        [DataMember]
+        int idCorreo;
+        [DataMember]
+        string correo;
+        [DataMember]
+        int Cuenta_idCuenta;
+
+        public Correo(int idCorreo, string correo, int cuenta_idCuenta)
+        {
+            this.idCorreo = idCorreo;
+            this.correo = correo;
+            Cuenta_idCuenta = cuenta_idCuenta;
+        }
+    }
+
+    [DataContract]
+    public class Telefono
+    {
+        [DataMember]
+        int idTelefono;
+        [DataMember]
+        string telefono;
+        [DataMember]
+        int Cuenta_idCuenta;
+
+        public Telefono(int idTelefono, string telefono, int cuenta_idCuenta)
+        {
+            this.idTelefono = idTelefono;
+            this.telefono = telefono;
+            Cuenta_idCuenta = cuenta_idCuenta;
+        }
+    }
+    [DataContract]
+    public class Genero
+    {
+        [DataMember]
+        int idGenero;
+        [DataMember]
+        string genero;
+
+        public Genero(int idGenero, string genero)
+        {
+            this.idGenero = idGenero;
+            this.genero = genero;
+           
+        }
+    }
+
+    [DataContract]
+    public class MensajeR
+    {
+        [DataMember]
+        Boolean error;
+
+        public MensajeR(bool error)
+        {
+            this.error = error;
+        }
+    }
+}
---
+++ ServiciosCuentaUsuario/ServicioCuentaUsuario.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+
+namespace ServiciosCuentaUsuario
+{
+   
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class ServicioCuentaUsuario : IServicioCuentaUsuario
+    {
+        int salida;
+        int salida2;
+        Cuenta cuenta;
+        public Cuenta IniciarSesion(string correo, string contrasena)
+        {
+            
+            MySqlCommand comando = new MySqlCommand(string.Format(
+                "Select Cuenta_idCuenta from Contrasena where contrasena='{0}'", contrasena), Conexion.ObtenerConexion());
+            MySqlDataReader reader = comando.ExecuteReader();
+            while (reader.Read())
+            {
+                salida = reader.GetInt32(0);
+            }
+
+            MySqlCommand comando2 = new MySqlCommand(string.Format(
+                "Select Cuenta_idCuenta from Correo where correo='{0}'", correo), Conexion.ObtenerConexion());
+            MySqlDataReader reader2 = comando2.ExecuteReader();
+            while (reader2.Read())
+            {
+                salida2 = reader2.GetInt32(0);
+            }
+            
+            if(salida == salida2)
+            {
+                MySqlCommand comando3 = new MySqlCommand(string.Format(
+                "Select idCuenta, nombreUsuario, idFotoCuentaUsuario ,Genero_idGenero from Cuenta where idCuenta='{0}'", salida), Conexion.ObtenerConexion());
+                MySqlDataReader reader3 = comando3.ExecuteReader();
+
+                while (reader3.Read())
+                {
+                    cuenta = new Cuenta(reader3.GetInt32(0),reader3.GetString(1),reader3.GetInt32(2),reader3.GetInt32(3));
+                }
+               
+                return cuenta;
+
+            }
+            return cuenta;
+        }
+
+        public MensajeR RegistrarUsuario(Cuenta cuenta, Contrasena contrasena, Correo correo, Telefono telefono)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
